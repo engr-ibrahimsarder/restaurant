10f8edf47a30fa943c1ccd9df7fd2175ae41ba21
unknown <ibrahimsarder969@gmail.com>
2025-01-16 17:19:27 +0600

    react hook form add

--- src/pages/home/Reviews.jsx
+++ src/pages/home/Reviews.jsx
@@ -1,19 +1,39 @@
 import { useEffect, useState } from "react";
-import Review from "./Review";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { RiDoubleQuotesL } from "react-icons/ri";
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([])
     useEffect(()=>{ 
         fetch('reviews.json')
-        .then(res=>res.json())
+        .then(res=>res.json())    
         .then(data=> {
             setReviews(data)
             console.log(data)
         })
-    },[]);
+    },[]); 
   return (
     <div>
-      <Review></Review>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {
+                reviews.map(review=>
+                    <SwiperSlide key={review._id}>
+
+                        <div className="text-center px-24 my-10">
+                            <div className="flex justify-center">
+                            <RiDoubleQuotesL className="text-xl" />
+                            </div>
+                        
+                           <p className="my-5">{review.details}</p> 
+                           <h1 className="text-xl">{review.name}</h1>
+                        </div>
+                    </SwiperSlide>
+                )
+            }
+      </Swiper>
     </div>
   );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,32 @@
-
+import login from "../../assets/others/authentication2.png"
+import { useForm } from "react-hook-form"
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
   return (
-    <div>
-      Login
+    <div className="flex">
+ <div>
+ <img src={login} alt="" />
+ </div>
+   <div>
+   <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+   </div>
     </div>
   );
 };
